fc74fc228be456f2420111ee9f2db2bc2612bf15
juhyeong <juhyeongee@gmail.com>
2022-07-09 16:57:52 +0900

    Feat: properties(=constants) 생성 & type선언

--- constants/Theme.tsx
+++ constants/Theme.tsx
@@ -1,10 +1,26 @@
+import { WINDOW_WIDTH, WINDOW_HEIGHT } from "./properties";
 // src/Theme.tsx
 export default {
   color: {
     white: "#FFFFFF",
     black: "#000000",
+    n0: "#FFFFFF",
+    n50: "#FAFAFA",
+    n100: "#F1F3F6",
+    n200: "#EBEEF2",
+    n300: "#E4E7ED",
+    n400: "#DCE0E8",
+    n500: "#D5DAE3",
+    n600: "#C7CCD4",
+    n700: "#A3A7AF",
+    n800: "#4E5054",
+    n900: "#333537",
+    mainGreen: "#40B08F",
   },
   fonts: {
-    normal: "14px",
+    mainFont: "NotoSansKR_400Regular",
+    thickFont: "NotoSansKR_700Bold",
+    thinFont: "NotoSansKR_100Thin",
   },
+  length: { windowWidth: WINDOW_WIDTH, windowHeight: WINDOW_HEIGHT },
 };
--- styled.d.ts
+++ styled.d.ts
@@ -4,8 +4,23 @@ interface ITheme {
   color: {
     white: string;
     black: string;
+    n0: string;
+    n50: string;
+    n100: string;
+    n200: string;
+    n300: string;
+    n400: string;
+    n500: string;
+    n600: string;
+    n700: string;
+    n800: string;
+    n900: string;
+    primary: string;
   };
   fonts: {
     normal: string;
+    mainFont: string;
+    thickFont: string;
+    thinFont: string;
   };
 }
