e4f04ec75ecb7a293167c0d63fd6ba0df6d9b6cd
juhyeong <juhyeongee@gmail.com>
2022-07-15 18:36:36 +0900

    feat: expo-google-font  Inter, noto-sans-kr 폰트설치

--- App.tsx
+++ App.tsx
@@ -4,12 +4,22 @@ import styled from "styled-components/native";
 import Theme from "./constants/Theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { IntroNav } from "./navigation/IntroNav";
+import MindTest from "./screens/MindTest";
+import {
+  useFonts,
+  NotoSansKR_100Thin,
+  NotoSansKR_300Light,
+  NotoSansKR_400Regular,
+  NotoSansKR_500Medium,
+  NotoSansKR_700Bold,
+  NotoSansKR_900Black,
+} from "@expo-google-fonts/noto-sans-kr";
 
 export default function App() {
   return (
     <NavigationContainer>
       <ThemeProvider theme={Theme}>
-        <IntroNav />
+        <MindTest />
       </ThemeProvider>
     </NavigationContainer>
   );
